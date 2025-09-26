4696c0f092e784a8b29bb908183000c4f3f7a91e
JustAADD <makilingadrian19@gmail.com>
2025-09-26 11:29:08 +0800

    initial commit

---
+++ appointment.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>D'new era appointment</title>
+
+    <!-- tailwindcss -->
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+    <link href="./output.css" rel="stylesheet">
+
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+</head>
+
+<body>
+
+    <div class="w-full h-full items-center justify-center">
+        <div class="grid grid-cols-2 items-center justify-center">
+
+        </div>
+    </div>
+
+</body>
+
+</html>
